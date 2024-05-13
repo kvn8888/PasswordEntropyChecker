0c4bc8fcca882dd370ac467851fe8ef75360e69b
Kevin Chen <kvn.c8888@gmail.com>
2024-05-13 16:11:57 -0400

    initial commit

---
+++ password-entropy/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Password Entropy Checker</h3>
+        <h5>Even through no data is saved, enter in fake passwords</h5>
+        <button>
+
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
