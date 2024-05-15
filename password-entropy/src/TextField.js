137a5239ea37d263b9743e2cf9e3375edd193ce5
Kevin Chen <kvn.c8888@gmail.com>
2024-05-15 19:08:57 -0400

    Added text input field

---
+++ password-entropy/src/TextField.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TextField.css';
+
+class TextField extends React.Component 
+{
+
+  render() 
+  {
+    return (
+      <input type="password" className='text-field'/>
+    )
+  }
+}
+
+export default TextField;
