5f09f43830463ef37152cdea0c04cb49c2768a78
Kevin Chen <kvn.c8888@gmail.com>
2024-05-16 15:21:01 -0400

    moved PasswordStrength and entropy state up from MotivationalCue to App

--- password-entropy/src/App.js
+++ password-entropy/src/App.js
@@ -8,12 +8,38 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        password: ''
+        password: '',
+        entropy: 0
         }
     }
 
+    PasswordStrength() {
+        let strength = 0;
+        let message;
+        const password = this.state.password;
+        const length = password.length;
+
+        const uppercase_regex = /[A-Z]/g;
+        const lowercase_regex = /[a-z]/g;
+        const digit_regex = /[0-9]/g;
+        const special_char_regex = /[~!@#$%^&*()_+{}|:"<>?`\-=[\]\\;',./]/g;
+
+        if(uppercase_regex.test(password)) {strength += 26;}
+        if(lowercase_regex.test(password)) {strength += 26;}
+        if(digit_regex.test(password)) {strength += 10;}
+        if(special_char_regex.test(password)) {strength += 34;}
+
+        const entropy = length * this.log2(strength);
+        this.setState({entropy: entropy});
+
+    }
+
+    log2(value) {
+        return Math.log(value) / Math.log(2);
+    }
+
     setPassword = (event) => {
-        this.setState({password: event.target.value});
+        this.setState({password: event.target.value}, () => {this.PasswordStrength(event.target.value)});
     }
 
     render() {
@@ -22,7 +48,7 @@ class App extends Component {
           <header className="App-header">
             <h3>Password Entropy Checker</h3>
             <h5>Even through no data is saved, enter in fake passwords</h5>
-            <MotivationalCue password={this.state.password}></MotivationalCue>
+            <MotivationalCue password={this.state.password} entropy={this.state.entropy}></MotivationalCue>
             <TextField setPassword={this.setPassword}></TextField>
           </header>
         </div>
--- password-entropy/src/MotivationalCue.js
+++ password-entropy/src/MotivationalCue.js
@@ -2,48 +2,8 @@ import React from "react";
 
 class MotivationalCue extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            entropy: 0
-        };
-    }
-
-    componentDidUpdate(prevProps) {
-        // Typical usage (don't forget to compare props):
-        if (this.props.password !== prevProps.password) {
-            this.PasswordStrength();
-        }
-    }
-
-
-    PasswordStrength() {
-        let strength = 0;
-        let message;
-        const password = this.props.password;
-        const length = password.length;
-
-        const uppercase_regex = /[A-Z]/g;
-        const lowercase_regex = /[a-z]/g;
-        const digit_regex = /[0-9]/g;
-        const special_char_regex = /[~!@#$%^&*()_+{}|:"<>?`\-=[\]\\;',./]/g;
-
-        if(uppercase_regex.test(password)) {strength += 26;}
-        if(lowercase_regex.test(password)) {strength += 26;}
-        if(digit_regex.test(password)) {strength += 10;}
-        if(special_char_regex.test(password)) {strength += 34;}
-
-        const entropy = length * this.log2(strength);
-        this.setState({entropy: entropy});
-
-    }
-
-    log2(value) {
-        return Math.log(value) / Math.log(2);
-    }
-
     message() {
-        const entropy = this.state.entropy;
+        const entropy = this.props.entropy;
 
         if (entropy <= 40) {return <h1>Yikes! That's a <italic>very</italic> bad password</h1>}
         else if (entropy <= 80) {return <h1>It would take a few seconds to crack your password</h1>}
