8f4c1ee2b119e7b14a6b9453fc368074cf156e17
Kevin Chen <kvn.c8888@gmail.com>
2024-07-07 16:43:14 -0400

    Update hash rate input and display calculations

--- password-entropy/src/TimeToBruteForce.js
+++ password-entropy/src/TimeToBruteForce.js
@@ -6,9 +6,9 @@ class TimeToBruteForce extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 1,
             activeTab: 'rtx 4090',
-            hash_rate:8_460_000_000
+            hash_rate:8_460_000_000,
+            customRate: ''
         };
     }
 
@@ -16,14 +16,40 @@ class TimeToBruteForce extends React.Component {
         this.setState({activeTab: tab});
     }
 
-    default_tabs() {
-        const { entropy } = this.props;
+    handleCustomChange = (event) => {
+        const customRate = event.target.value;
+        this.setState({
+            hash_rate: customRate
+        });
+    }
 
-        return(
-            <div>
+    convertHashRate = (hashRate) => {
+        if (hashRate >= 1e12) {
+            return (hashRate/1e12).toFixed(2) + ' TH/s'
+        } else if (hashRate>=1e9) {
+            return (hashRate/1e9).toFixed(2) + ' GH/s'
+        } else if (hashRate>=1e6) {
+            return (hashRate/1e6).toFixed(2) + ' MH/s'
+        } else {
+            return hashRate + ' H/s';
+        }
+    }
 
-            </div>
-        )
+    timeToBruteforce = () => {
+        const totalCombinations = Math.pow(2, this.props.entropy);
+        const secondsToCrack = totalCombinations / this.state.hash_rate;
+        const daysToCrack = secondsToCrack / (60 * 60 * 24);
+        if(daysToCrack > 30) {
+            const monthsToCrack = daysToCrack/30.436875;
+            if(monthsToCrack>12) {
+                return (monthsToCrack/12).toFixed(2) + " years";
+            }
+            return monthsToCrack.toFixed(2) + " months";
+        }
+        if(daysToCrack >= 1) {
+            return daysToCrack.toFixed(2) + " days";
+        }
+        return secondsToCrack.toFixed(2) + " seconds";
     }
 
     render() {
@@ -32,9 +58,13 @@ class TimeToBruteForce extends React.Component {
             <div className="Tool">
                 <h3>Time To Bruteforce</h3>
                 <div className="input-container">
-                    <input className="Count" type="number" placeholder="Count" onChange={event => {
-                        this.setState({count: event.target.value})
-                    }}/>
+                    <input
+                        className="Count"
+                        type="number"
+                        placeholder="Custom Hash Rate"
+                        onChange={this.handleCustomChange}
+                        value={this.state.hash_rate}
+                    />
                     <div className="tabs">
                         <button className={this.state.activeTab === "rtx 4090" ? 'active' : ''} onClick={() => {
                             this.setHardware('rtx 4090');
@@ -47,24 +77,13 @@ class TimeToBruteForce extends React.Component {
                             this.setHardware('antminer s19 pro');
                             this.setState({hash_rate: 110_000_000_000_000})
                         }}>
-                            antminer s19 pro
+                            s19 pro
                         </button>
-                        <button className={this.state.activeTab === "custom" ? 'active' : ''} onClick={() => {
-                            this.setHardware('custom')
-                        }}>Custom
-                        </button>
-                    </div>
-                    <div className="tab-content">
-                        {this.state.setHardware === 'rtx 4090' && <div>{this.default_tabs()}</div>}
-                        {this.state.setHardware === 'antminer s19 pro' && <div>{this.default_tabs()}</div>}
-                        {this.state.setHardware === 'custom' && <div><label>
-                            <input className="CustomEntropy" type="number" onChange={(event) => {
-                                this.setState({entropy_specification: event.target.value})
-                            }}/>
-                        </label>{this.default_tabs()}</div>}
+
                     </div>
                 </div>
-                {/*<p></p>*/}
+                <p className="hash-rate-display">{this.convertHashRate(this.state.hash_rate)}</p>
+                <p>It would take {this.timeToBruteforce()} to brute force your password with a {this.state.activeTab}</p>
             </div>
         )
     }
